81908c4aa331d03c8e93b6522466dbef25fb91fd
d4ny4z0rd <42492143+d4ny4z0rd@users.noreply.github.com>
2024-01-23 18:42:42 +0530

    Update hero-section.tsx

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
 			<div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
 				<div className="md:mt-2 md:w-1/2">
 					<Image
-						src="/20240108_150001.jpg"
+						src="/naruto.jpeg"
 						alt=""
 						width={325}
 						height={325}
